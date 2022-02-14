10d081df4004ffd7836b3d89f23951dfc52cd575
khs <69028828+happysang@users.noreply.github.com>
2022-02-14 17:58:56 +0900

    Update 동적계획법.py

--- 동적계획법.py
+++ 동적계획법.py
@@ -10,3 +10,17 @@ def solution(t):
                 t[i+1][j] += t[i][j+1]
                 
     return t[-1][-1]
+
+
+### 등굣길
+def solution(m, n, p):
+    way = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    way[0][1] = 1
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if [j,i] in p:
+                way[i][j] = 0
+            else:
+                way[i][j] = (way[i - 1][j] + way[i][j - 1]) % 1000000007
+                
+    return way[-1][-1]
