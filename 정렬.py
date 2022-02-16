97fd8bf3faf9e0f39ebb5d075bc52f6834ab02f4
khs <69028828+happysang@users.noreply.github.com>
2022-02-17 02:01:09 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -7,15 +7,4 @@ def solution(a, c):
 
 ### 가장 큰 수
 def solution(n):
-    nums = sorted(list(map(str,n)),reverse=True)
-    tar = []
-    temp = [nums[0]]
-    for i in range(len(nums)):
-        if nums[i][0] == nums[i-1][0]:
-            temp.append(nums[i])
-            
-    print(nums)
-    answer = ''
-    for i in nums:
-        answer += i   
-    return answer
+    return str(int(''.join(sorted(list(map(str,n)), key = lambda x : x*3, reverse = True))))
