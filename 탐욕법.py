6af87cfb3b8dd74a99dc05141bea7078fec5fadc
khs <69028828+happysang@users.noreply.github.com>
2022-02-23 06:40:48 +0900

    Update 탐욕법.py

--- 탐욕법.py
+++ 탐욕법.py
@@ -28,3 +28,43 @@ def solution(n, lost, reserve):
     return len([x for x in stud if x != 0])-1
     
     
+    ### 조이스틱
+    def solution(name):
+        res1 = -1
+    tar1 = ''
+    for x in name:
+        if x != 'A':
+            tar1 += x
+    temp1 = ''
+    
+    for x in range(len(name)):
+        res1 += 1
+        if temp1 == tar1:
+            break
+        if ord(name[x]) <= 77:
+            res1 += (ord(name[x])-65)
+        else:
+            res1 += (90-ord(name[x])+1)
+        temp1 += name[x]
+            
+    
+    res2 = -1
+    tar2 = ''
+    name = name[0]+name[len(name):0:-1]
+    for x in name:
+        if x != 'A':
+            tar2 += x
+    temp2 = ''
+    
+    for x in range(len(name)):
+        res2 += 1
+        if temp2 == tar2:
+            break
+        if ord(name[x]) <= 77:
+            res2 += (ord(name[x])-65)
+        else:
+            res2 += (90-ord(name[x])+1)
+        temp2 += name[x]
+
+    return min(res1, res2)
+    
