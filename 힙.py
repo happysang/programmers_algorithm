2445fdf61628cd758dff6c1eca96aeb6ea134c32
khs <69028828+happysang@users.noreply.github.com>
2022-03-01 23:59:25 +0900

    Update 힙.py

--- 힙.py
+++ 힙.py
@@ -14,3 +14,19 @@ def solution(s, k):
         cnt += 1
     
     return cnt
+
+
+### 디스크 컨트롤러
+import heapq as hp
+def solution(jobs):
+    nums = []
+    for x in jobs:
+        hp.heappush(nums, [x[1],x[0]])
+    
+    print(nums)
+    
+    res = 0
+    for _ in range(len(jobs)):
+        res += hp.heappop(nums)[1]
+        print(res)
+        
